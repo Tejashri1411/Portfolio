3f94e7770e9efaa8ead28d897f316f81b85dd925
Tejashri1411 <tejashridhomkar2003@gmail.com>
2025-05-16 20:34:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+setTimeout(() => {
+    var typed = new Typed(".multiple-text", {
+        strings: ["MCS Student.","SOFTWARE DEVELOPER.","FULL STACK DEVELOPER","Problem Solver."],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    })
+}, 4000);
